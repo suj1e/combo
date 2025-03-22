57737597269c937f17dc3ef205271417c8218003
sujie <13156660820@163.com>
2025-03-23 01:14:37 +0800

    调整

--- combo-project/combo-x/src/main/java/org/flooc/combo/x/web/logging/AppLoggingRegistrar.java
+++ combo-project/combo-x/src/main/java/org/flooc/combo/x/web/logging/AppLoggingRegistrar.java
@@ -1,13 +1,15 @@
 package org.flooc.combo.x.web.logging;
 
-import java.util.Map;
+import java.lang.annotation.Annotation;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.cloud.openfeign.FeignClientSpecification;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.lang.NonNull;
+import org.springframework.util.StringUtils;
 
 /**
  * @author sujie
@@ -16,20 +18,32 @@ import org.springframework.lang.NonNull;
 class AppLoggingRegistrar implements ImportBeanDefinitionRegistrar {
 
   @Override
-  public void registerBeanDefinitions(AnnotationMetadata metadata,
+  public void registerBeanDefinitions(@NonNull AnnotationMetadata metadata,
       @NonNull BeanDefinitionRegistry registry) {
-    Map<String, Object> attrs = metadata.getAnnotationAttributes(EnableAppLogging.class.getName(),
-        true);
-    if (attrs == null) {
+    AnnotationAttributes attributes = getAttributes(metadata);
+    if (attributes == null) {
       return;
     }
-    String value = String.valueOf(attrs.get("value"));
+    String pkg = String.valueOf(attributes.get("value"));
+    if (!StringUtils.hasText(pkg)) {
+      String className = metadata.getClassName();
+      pkg = className.substring(0, className.lastIndexOf("."));
+    }
     BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(
         FeignClientSpecification.class);
-    builder.addConstructorArgValue(value);
+    builder.addConstructorArgValue(pkg);
     builder.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
     registry.registerBeanDefinition(AppLoggingConfiguration.class.getSimpleName(),
         builder.getBeanDefinition());
+  }
+
+  protected AnnotationAttributes getAttributes(AnnotationMetadata metadata) {
+    String name = getAnnotationClass().getName();
+    return AnnotationAttributes.fromMap(metadata.getAnnotationAttributes(name, true));
+  }
+
+  private Class<? extends Annotation> getAnnotationClass() {
+    return EnableAppLogging.class;
   }
 
 }
