ae754d43f141ca68eef1aa5c9cf2919e238448ec
sujie <13156660820@163.com>
2025-04-13 13:40:46 +0800

    refactor(CodeGenExecutor): 重构代码生成执行器
    static块加载生成器map

--- combo-project/combo-multiple/combo-mft-codegen/src/main/java/org/flooc/combo/mft/codegen/core/CodegenExecutor.java
+++ combo-project/combo-multiple/combo-mft-codegen/src/main/java/org/flooc/combo/mft/codegen/core/CodegenExecutor.java
@@ -4,7 +4,6 @@ import com.google.common.base.Preconditions;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.ServiceLoader.Provider;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,8 +16,15 @@ import org.flooc.combo.mft.codegen.exception.CodegenException;
  * @since 1.0.0
  */
 public class CodegenExecutor {
-  // TODO 使用静态块来初始化，保证第一次使用时被初始化且只被初始化一次
+
   private static final Map<CodegenEntryType, CodegenEntry> CODEGEN_ENTRY_CACHE = new ConcurrentHashMap<>();
+
+  static {
+    ServiceLoader<CodegenEntry> sl = ServiceLoader.load(CodegenEntry.class,
+        CodegenExecutor.class.getClassLoader());
+    sl.stream().filter(Objects::nonNull).map(Provider::get).filter(Objects::nonNull)
+        .forEach(i -> CODEGEN_ENTRY_CACHE.put(i.type(), i));
+  }
 
   public static void executeIdeaPlugin(CodegenContext context) throws IOException {
     execute(CodegenEntryType.IdeaPlugin, context);
@@ -26,27 +32,10 @@ public class CodegenExecutor {
 
   public static void execute(CodegenEntryType type, CodegenContext context) throws IOException {
     Preconditions.checkNotNull(type);
-    if (CODEGEN_ENTRY_CACHE.containsKey(type)) {
-      CODEGEN_ENTRY_CACHE.get(type).generate(context);
-      return;
-    }
-    ServiceLoader<CodegenEntry> sl = ServiceLoader.load(CodegenEntry.class,
-        CodegenExecutor.class.getClassLoader());
     if (CODEGEN_ENTRY_CACHE.isEmpty()) {
-      sl.stream().filter(Objects::nonNull).map(Provider::get).filter(Objects::nonNull)
-          .forEach(i -> CODEGEN_ENTRY_CACHE.put(i.type(), i));
+      throw new CodegenException("Failed to generate, generator is empty");
     }
-    Optional<CodegenEntry> entryOptional = sl.stream().filter(Objects::nonNull).map(Provider::get)
-        .filter(Objects::nonNull)
-        .filter(i -> type.equals(i.type()))
-        .findFirst();
-    if (entryOptional.isPresent()) {
-      CodegenEntry codegenEntry = entryOptional.get();
-      CODEGEN_ENTRY_CACHE.put(type, codegenEntry);
-      codegenEntry.generate(context);
-    } else {
-      throw new CodegenException("生成器为空");
-    }
+    CODEGEN_ENTRY_CACHE.get(type).generate(context);
   }
 
 }
